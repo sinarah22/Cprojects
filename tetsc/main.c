b9c5dbd9c0c43f0447552c89b45f6b768a368f1f
sinarah22 <sinarahmannejad@itdcanada.ca>
2019-07-10 08:56:09 -0700

    changes in testc file

--- tetsc/main.c
+++ tetsc/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 int factorial(int n);
+int fibo(int n);
 int main()
 {
 //    //make space between charcters
@@ -13,17 +14,34 @@ int main()
 //        j+=1;
 //    }
 //**********************************************************
-for(int=1;i<6;i+=1){
-    printf("factorial %d is %d= %d \n",i,i,factorial(i));}
+//for(int=1;i<6;i+=1){
+//    printf("factorial %d is %d= %d \n",i,i,factorial(i));}
+//****************************************************
+//for (int i=1;i<20;i+=1){
+//    printf("fibo %d : %d \n",i,fibo(i));
+//}
 
 
+//******************
 
-    return 0;
+
+   return 0;
 }
-int factorial(int n){
-if(n==1){
-    return 1;
-}
-return n*factorial(n-1);
+//int factorial(int n){
+//if(n==1){
+//    return 1;
+//}
+//return n*factorial(n-1);
+//
+//}
+//****************
+//int fibo(int n){
+//    if(n==1){
+//        return 0;
+    }
+//if (n==2){
+//return 1;}
 
-}
+//return fibo(n-1)+fibo(n-2);
+//}
+***************************
