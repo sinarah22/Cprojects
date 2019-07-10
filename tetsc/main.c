02634ccee7841989bf709315fae82f9cfdd30ea8
sinarah22 <sinarahmannejad@itdcanada.ca>
2019-07-10 10:55:51 -0700

    pointer function added to testc project

--- tetsc/main.c
+++ tetsc/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define size 10
 int factorial(int n);
 int fibo(int n);
 int main()
@@ -22,9 +23,19 @@ int main()
 //}
 
 
+
+//}
+//*****************
+float numbers[size]= {0};
+float *poinee;
+poinee= numbers;
+for(int i=2;i<10;i+=2){
+    *poinee=i;
+    printf("pointer %d, value %.1f \n",1+i/2,*poinee);
+    poinee+=1;
+
+}
 //******************
-
-
    return 0;
 }
 //int factorial(int n){
@@ -38,10 +49,10 @@ int main()
 //int fibo(int n){
 //    if(n==1){
 //        return 0;
-    }
+//    }
 //if (n==2){
 //return 1;}
 
 //return fibo(n-1)+fibo(n-2);
 //}
-***************************
+//***************************
