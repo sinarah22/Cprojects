e3413921a01af2f14fd2f427aa508205c7c94e70
sinarah22 <sinarahmannejad@itdcanada.ca>
2019-07-10 11:49:21 -0700

    Strcpy new function to use when have a char in out strc and codebloc shows a bug .bla.bla.bla

--- tetsc/main.c
+++ tetsc/main.c
@@ -3,6 +3,12 @@
 #define size 10
 int factorial(int n);
 int fibo(int n);
+struct student{
+int id;
+char *name[20];
+float gpa;
+int term;
+};
 int main()
 {
 //    //make space between charcters
@@ -26,16 +32,40 @@ int main()
 
 //}
 //*****************
-float numbers[size]= {0};
-float *poinee;
-poinee= numbers;
-for(int i=2;i<10;i+=2){
-    *poinee=i;
-    printf("pointer %d, value %.1f \n",1+i/2,*poinee);
-    poinee+=1;
+//float numbers[size]= {0};
+//float *poinee;
+//poinee= numbers;
+//for(int i=2git;i<10;i+=2){
+//    *poinee=i;
+//    printf("pointer %d, value %.1f \n",1+i/2,*poinee);
+//    poinee+=1;
+//}
+//******************
+struct student student1;
+student1.id=100;
+//student1.name="Sina";
+//this down code is because the code up name is a char and does not work with the code up
+strcpy( student1.name, "Sara" );
+student1.gpa; 4.0;
+student1.term= 3;
+
+int student[3];
+for(int i=1;i<=3;i++){
+puts("type Id");
+scanf("%d",&student1.id);
+puts("type name");
+scanf("%s",&student1.name);
+puts("type gpa");
+scanf("%f",&student1.gpa);
+puts("type term");
+scanf("%d",&student1.term);
+printf("student %d name is : %s",i,student1.name);
+printf("\nstudent %d id is : %d",i,student1.id);
+printf("\nstudent %d name is : %.2f",i,student1.gpa);
+printf("\nstudent %d name is : %d\n",i,student1.term);
+student[i];
 
 }
-//******************
    return 0;
 }
 //int factorial(int n){
